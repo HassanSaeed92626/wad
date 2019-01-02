e4a97b2d784951d00ede5548d69955cc7a3433ce
HassanSaeed92626 <43633069+HassanSaeed92626@users.noreply.github.com>
2019-01-03 00:05:25 +0500

    updated functions.php

--- pro/server/functions.php
+++ pro/server/functions.php
@@ -1,23 +1,29 @@
 <?php
-require_once "db_connection.php";
-
-function getCats(){
-    global $con;
-    $getCatsQuery = "select * from categories";
-    $getCatsResult = mysqli_query($con,$getCatsQuery);
-    while($row = mysqli_fetch_assoc($getCatsResult)){
-        $cat_id = $row['cat_id'];
-        $cat_title = $row['cat_title'];
-        echo "<li><a class='nav-link'  href='#'>$cat_title</a></li>";
-    }
+require "db_connection.php";
+function getcats(){
+	global $con;
+	
+	$getCatQuery="select * from categories";
+	$result = mysqli_query($con,$getCatQuery);
+	while($row=mysqli_fetch_assoc($result)){
+	$title=$row['cat_title'];
+	$id= $row['cat_id'];
+	echo"<li>
+                        <a class='nav-link'  href='#'> $title </a>
+                    </li>";
+	}
 }
-function getBrands(){
-    global $con;
-    $getBrandsQuery = "select * from brands";
-    $getBrandsResult = mysqli_query($con,$getBrandsQuery);
-    while($row = mysqli_fetch_assoc($getBrandsResult)){
-        $brand_id = $row['brand_id'];
-        $brand_title = $row['brand_title'];
-        echo "<li><a class='nav-link'  href='#'>$brand_title</a></li>";
-    }
+function getbrands(){
+	global $con;
+	
+	$getBrandQuery="select * from brands";
+	$result = mysqli_query($con,$getBrandQuery);
+	while($row=mysqli_fetch_assoc($result)){
+	$title=$row['brand_title'];
+	$id= $row['brand_id'];
+	echo"<li>
+                        <a class='nav-link'  href='#'> $title </a>
+                    </li>";
+	}
 }
+?>
