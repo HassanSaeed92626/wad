4dc147d351da42d79014d735e87d2a7dbd93fb23
HassanSaeed92626 <43633069+HassanSaeed92626@users.noreply.github.com>
2019-01-25 18:57:37 +0500

    Quiz8 Updated

--- pro/index.php
+++ pro/index.php
@@ -1,8 +1,7 @@
-<?php
-include "server/functions.php"
-?>
-
 <!DOCTYPE html>
+<?php
+require "server/functions.php";
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,13 +12,32 @@ include "server/functions.php"
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
 </head>
+        <script>
+            function displayProduct(str) {
+                if (str.length == 0) {
+                    document.getElementById("hint").innerHTML = "";
+                    return;
+                }
+                else {
+                    var xmlhttp = new XMLHttpRequest();
+                    xmlhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                            document.getElementById("pros").innerHTML = this.responseText;
+                        }
+                    };
+                    xmlhttp.open("GET", "show_product.php?e=" + str, true);
+                    xmlhttp.send();
+                    //document.getElementById('hint').innerHTML = 'loading...';
+                }
+            }
+        </script>
 <body>
 
 <header class="container-fluid">
     <div class="row">
         <div class="col-12 no-padding">
             <nav class="navbar navbar-light bg-light navbar-expand-sm fixed-top">
-                <a class="navbar-brand" href="index.html"><img src="media/logo.png" width="175" height="50" alt="logo">
+                <a class="navbar-brand" href="index.php"><img src="media/logo.png" width="175" height="50" alt="logo">
                 </a>
                 <button class="navbar-toggler" type="button"
                         data-toggle="collapse"
@@ -32,9 +50,10 @@ include "server/functions.php"
                             <div class="input-group">
                                 <input type="search" class="form-control"
                                        id="search-bar" name="search"
-                                       placeholder="Find Mobile Phones, Laptops, and more..">
+                                       placeholder="Find Mobile Phones, Laptops, and more.."
+                                    onkeyup="displayProduct(this.value)">
                                 <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary btn-lg" type="button"><i class="fas fa-search"></i></button>
+                                    <button class="btn btn-outline-secondary btn-lg" type="submit"><i class="fas fa-search"></i></button>
                                 </div>
                             </div>
                         </form>
@@ -68,11 +87,7 @@ include "server/functions.php"
                     Categories
                 </a>
                 <ul class="collapse show list-unstyled" id="homeSubmenu">
-                    <?php
-				
-					getcats();
-					
-					?>
+                    <?php getCats(); ?>
                 </ul>
             </li>
             <li class="active">
@@ -81,11 +96,7 @@ include "server/functions.php"
                     Brands
                 </a>
                 <ul class="collapse show list-unstyled" id="pageSubmenu">
-					<?php
-						
-						getbrands();
-					
-					?>
+                    <?php getBrands(); ?>
                 </ul>
             </li>
             <li>
@@ -104,10 +115,8 @@ include "server/functions.php"
     </nav>
     <article id="content" class="container-fluid bg-white">
 
-        <div class="row">
-            <div class="col">
-                Content
-            </div>
+        <div id="pros" class="row">
+                <?php getPro(); ?>
         </div>
     </article>
 
